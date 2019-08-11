662c69b8c6221e03eca7b2335b67ba1f55e4656d
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-11 16:44:05 +0000

    added pry gem

--- lib/legend_scraper.rb
+++ lib/legend_scraper.rb
@@ -14,13 +14,13 @@ def stat_scraper
   
   legends = {}
  
-  legend_stats.css("li.project.grid_4").each do |legend|
+  legend_stats.css("div.box___jUrgl.stats___8TQpl").each do |legend|
     name = legend.css("h2.bbcard_name strong a").text
     legends[name.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :image_link => legend.css("div.project-thumbnail a img").text.gsub("%","").to_i,
+      :description => legend.css("p.bbcard_blurb").text.gsub("%","").to_i,
+      :location => legend.css("ul.project-meta span.location-name").text.gsub("%","").to_i,
+      :percent_funded => legend.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
   
