a62a019a4daa706204a4da303ad0f193f9f62606
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-11 15:25:01 +0000

    further work on scraper

--- lib/legend_scraper.rb
+++ lib/legend_scraper.rb
@@ -1,12 +1,30 @@
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
-require 'rake'
 
-class initial_scrape
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta li span.location-name").text
+# percentage_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+def stat_scraper
+  html = File.read('https://dreamteam.gg/apex/legends/pathfinder')
+  legend_stats = Nokogiri::HTML(html)
   
-  def scrape_data
-  doc1 = Nokogiri::HTML(open("https://dreamteam.gg/apex/legends/pathfinder"))
-  end 
+  legends = {}
+ 
+  legend_stats.css("li.project.grid_4").each do |legend|
+    name = legend.css("h2.bbcard_name strong a").text
+    legends[name.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
   
-end 
+  legends
+end
+
+stat_scraper 
