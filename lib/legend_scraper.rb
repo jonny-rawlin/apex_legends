bade52d1af3537895362fddbe165f88ec77b5a98
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-11 17:42:58 +0000

    edit to scraper

--- lib/legend_scraper.rb
+++ lib/legend_scraper.rb
@@ -2,13 +2,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta li span.location-name").text
-# percentage_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
 def stat_scraper
   legend_stats = Nokogiri::HTML(open("https://dreamteam.gg/apex/legends/pathfinder"))
   
