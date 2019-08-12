ae14b5b21859bd5ee677234b416487fdb6852e6b
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-12 16:05:36 +0000

    switched to using selenium-webdriver for scraping due to javascript issue

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
 gem 'pry'
-gem 'nokogiri'
 gem 'rspec'
+gem 'selenium-webdriver'
--- lib/legend_scraper.rb
+++ lib/legend_scraper.rb
@@ -1,25 +1,37 @@
 require 'open-uri'
-require 'nokogiri'
+require 'selenium-webdriver'
 require 'pry'
 
-def stat_scraper
-  legend_stats = Nokogiri::HTML(open("https://dreamteam.gg/apex/legends/pathfinder"))
-  
-  legends = {}
- 
-  legend_stats.css("div.box___jUrgl.stats___8TQpl").each do |legend|
-    name = legend.css("span div.box___jUrgl.item___29sDA.legend___28WrD").text
-    legends[name.to_sym] = {
-    #   :percentage_chosen => legend.css("").text.gsub("%","").to_i,
-    #   :win_rate => legend.css("").text.gsub("%","").to_i,
-    #   :damage_per_match => legend.css("").text.gsub("%","").to_i,
-    #   :kills_per_match => legend.css("").text.gsub("%","").to_i,
-    #   :headshots_per_kill => legend.css("").text.gsub("%","").to_i
-    }
-    binding.pry
-  end
-  
-  legends
-end
+require 'selenium-webdriver'
 
-stat_scraper 
+options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
+
+driver = Selenium::WebDriver.for(:chrome, options: options)
+
+driver.get('http://stackoverflow.com/')
+
+puts driver.title
+
+driver.quit
+
+# def stat_scraper
+#   legend_stats = Nokogiri::HTML(open("https://dreamteam.gg/apex/legends/pathfinder"))
+  
+#   legends = {}
+ 
+#   legend_stats.css("div.box___jUrgl.stats___8TQpl").each do |legend|
+#     name = legend.css("span div.box___jUrgl.item___29sDA.legend___28WrD").text
+#     legends[name.to_sym] = {
+#     #   :percentage_chosen => legend.css("").text.gsub("%","").to_i,
+#     #   :win_rate => legend.css("").text.gsub("%","").to_i,
+#     #   :damage_per_match => legend.css("").text.gsub("%","").to_i,
+#     #   :kills_per_match => legend.css("").text.gsub("%","").to_i,
+#     #   :headshots_per_kill => legend.css("").text.gsub("%","").to_i
+#     }
+#     binding.pry
+#   end
+  
+#   legends
+# end
+
+# stat_scraper 
