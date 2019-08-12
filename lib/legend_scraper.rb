8ab8de2cc66e11be6687a1d31e9f23184bc9d5ff
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-12 18:47:19 +0000

    updates to scraper

--- lib/legend_scraper.rb
+++ lib/legend_scraper.rb
@@ -6,9 +6,11 @@ def stat_scraper
   legend_stats = Nokogiri::HTML(open("https://dreamteam.gg/apex/legends/pathfinder"))
   
   legends = {}
- 
-  legend_stats.css("div.content___TWkBZ").each do |legend|
-    name = legend.css("div").text
+  names = legend_stats.css("p.dynamicSidebarItemTitle___-SjTz.labelL___qgsCI.condensed___28u8b")[5..-1]
+  
+  legend_stats.css("div.general___3NxDu").each_with_index do |legend, i|
+    #name = legend.css("p[breed='title']").text
+    name = names[i].text
     legends[name.to_sym] = {
     #   :percentage_chosen => legend.css("").text.gsub("%","").to_i,
     #   :win_rate => legend.css("").text.gsub("%","").to_i,
