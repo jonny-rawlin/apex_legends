455fd3183b14ae4d3dfb53512c8d49fec5d94f12
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-13 11:44:28 +0000

    updated class dependencies

--- lib/apex_legends/legends.rb
+++ lib/apex_legends/legends.rb
@@ -1,33 +1,37 @@
-class ApexLegends::Legends
+require 'pry'
+require_relative './legend_scraper.rb'
+
+class Legends
+  include LegendScraper
   
   attr_accessor :name, :stats, :abilities, :bio 
   
   def self.all_legends
-    legend_1 = self.new 
-    legend_1.name = "Wraith"
-    legend_1.stats = "Random stats"
-    legend_1.abilities = "Three abilities"
-    legend_1.bio = "Interesting facts"
+    # legend_1 = self.new 
+    # legend_1.name = "Wraith"
+    # legend_1.stats = "Random stats"
+    # legend_1.abilities = "Three abilities"
+    # legend_1.bio = "Interesting facts"
     
-    legend_2 = self.new 
-    legend_2.name = "Bangalor"
-    legend_2.stats = "Random stats"
-    legend_2.abilities = "Three abilities"
-    legend_2.bio = "Interesting facts"
+    # legend_2 = self.new 
+    # legend_2.name = "Bangalor"
+    # legend_2.stats = "Random stats"
+    # legend_2.abilities = "Three abilities"
+    # legend_2.bio = "Interesting facts"
     
-    legend_3 = self.new 
-    legend_3.name = "Pathfinder"
-    legend_3.stats = "Random stats"
-    legend_3.abilities = "Three abilities"
-    legend_3.bio = "Interesting facts"
+    # legend_3 = self.new 
+    # legend_3.name = "Pathfinder"
+    # legend_3.stats = "Random stats"
+    # legend_3.abilities = "Three abilities"
+    # legend_3.bio = "Interesting facts"
     
-    legend_4 = self.new 
-    legend_4.name = "Bloodhound"
-    legend_4.stats = "Random stats"
-    legend_4.abilities = "Three abilities"
-    legend_4.bio = "Interesting facts"
+    # legend_4 = self.new 
+    # legend_4.name = "Bloodhound"
+    # legend_4.stats = "Random stats"
+    # legend_4.abilities = "Three abilities"
+    # legend_4.bio = "Interesting facts"
     
-    [legend_1, legend_2, legend_3, legend_4]
+    # [legend_1, legend_2, legend_3, legend_4]
   end 
-  
+  binding.pry 
 end 
