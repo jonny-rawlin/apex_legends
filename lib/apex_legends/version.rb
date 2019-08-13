34a94b8032a47230b1be26d9f6a7e7eb41859a8d
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-13 14:25:51 +0000

    removed require from version file

--- lib/apex_legends/version.rb
+++ lib/apex_legends/version.rb
@@ -1,7 +1,6 @@
-require_relative '../lib/apex_legends/version'
-require_relative '../lib/apex_legends/cli'
-require_relative '../lib/apex_legends/legends'
-require_relative '../lib/apex_legends/legend_scraper'
+# require_relative '../lib/apex_legends/cli'
+# require_relative '../lib/apex_legends/legends'
+# require_relative '../lib/apex_legends/legend_scraper'
 
 module ApexLegends
   VERSION = "0.1.0"
