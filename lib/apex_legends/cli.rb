59046e580b4aa7eef04ef1389086d88d94ef37b5
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-14 14:21:01 +0000

    final adjustments to cli methods

--- lib/apex_legends/cli.rb
+++ lib/apex_legends/cli.rb
@@ -29,7 +29,7 @@ class ApexLegends::CLI < Legends
   end 
   
   def menu
-    puts "\r\nTo find out more about a Legend from the list above, simply enter their number. You can type 'exit' at any time to exit the program"
+    puts "\r\nAbove are all of the available Legends' stats - you may need to scroll up to see view them all.\r\n\r\nTo find out more about a Legend from the list above, simply enter their number.\r\n\r\nYou can also type 'exit' at any time to leave the program"
     input = nil 
     while input != "exit"
       input = gets.strip.downcase
@@ -42,6 +42,10 @@ class ApexLegends::CLI < Legends
         puts "\r\n#{bio} -\r\n\r\nIf you'd like to select this Legend then type 'yes' to confirm. You can also type 'list legends' to see all of the options again."
       elsif input == "list legends"
         puts @legends
+      elsif input == "yes"
+        puts "\r\nThat's a great choice! You can now type 'exit' to leave the program"
+      else 
+        puts "\r\nI'm sorry, I didn't understand that. You can type 'list legends' if you need to see the list again."
       end 
     end 
   end 
