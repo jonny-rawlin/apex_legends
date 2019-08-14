3717ae0e93fcf89596fc1ae05b8233b44dd6d9ca
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-08-14 11:38:45 +0000

    updated formating of cli output to console

--- lib/apex_legends/cli.rb
+++ lib/apex_legends/cli.rb
@@ -2,34 +2,40 @@ require_relative "./legends.rb"
 
 class ApexLegends::CLI < Legends
   
-  def call 
+  def call
+    welcome
     list_legends
     menu
     goodbye 
   end
   
+  def welcome
+    puts "Welcome to King's Landing!"
+  end 
+  
   def list_legends
-    puts "Available Legends:"
+    puts "\r\nThese are your available Legends:"
     @stat_list = Legends.stat_lister
     @legends = Legends.all_legends
     @stat_list.each do | legend, stat_title|
-     puts "#{legend}:"
+     puts "\r\n#{legend}:"
       stat_title.each do |stat_title, value|
        puts "  #{stat_title.to_s.split("_").map {|stat| stat.capitalize}.join(" ")}: #{value}"
       end
     end
+    puts "\r\n"
     puts @legends
   end 
   
   def menu
-    puts "Select the Legend you'd like to know more about by entering their number. Type 'exit' at any time to exit the program"
+    puts "\r\nTo find out more about a Legend from the list above, simply enter their number. You can type 'exit' at any time to exit the program"
     input = nil 
     while input != "exit"
       input = gets.strip.downcase
       
       if input.to_i > 0
         the_legend = (@legends[input.to_i-1]).split(" ")
-        puts "#{the_legend[1]} -\r\n\r\n- If you'd like to select this Legend then type their name below. You can also type 'list legends' to see all of the options again."
+        puts "\r\n#{the_legend[1]} -\r\n\r\nIf you'd like to select this Legend then type their name below. You can also type 'list legends' to see all of the options again."
       elsif input == "list legends"
         puts @legends
       end 
@@ -37,7 +43,7 @@ class ApexLegends::CLI < Legends
   end 
   
   def goodbye
-    puts "Enjoy your time in Kings Landing..."
+    puts "\r\nEnjoy your time in Kings Landing..."
   end 
   
-end 
+end
